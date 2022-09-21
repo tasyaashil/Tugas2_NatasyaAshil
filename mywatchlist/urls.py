b370172abc2b29b7dfdf4224863d53c97d9c7405
natasyaashil <natasya.ashil@ui.ac.id>
2022-09-22 00:29:51 +0700

    bismillah bisa

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_mywatchlistxml, name='show_mywatchlistxml'), 
     path('json/', show_mywatchlistjson, name='show_wishlistjson'), 
-
 ]
