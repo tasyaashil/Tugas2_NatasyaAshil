f26904a029f124b3b3c3b3151dfa93b33ffc07a7
natasyaashil <natasya.ashil@ui.ac.id>
2022-09-22 00:42:04 +0700

    hai

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,17 +1,17 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-# Create your tests here.
-class jsonTests(TestCase):
-    def tests_testjsonpage(self):
-        response = self.client.get('/mywatchlist/json/')
-        self.assertEqual(response.status_code, 200)
+# # Create your tests here.
+# class jsonTests(TestCase):
+#     def tests_testjsonpage(self):
+#         response = self.client.get('/mywatchlist/json/')
+#         self.assertEqual(response.status_code, 200)
         
-class xmlTests(TestCase):
-    def tests_testxmlpage(self):
-        response = self.client.get('/mywatchlist/xml/')
-        self.assertEqual(response.status_code, 200)
+# class xmlTests(TestCase):
+#     def tests_testxmlpage(self):
+#         response = self.client.get('/mywatchlist/xml/')
+#         self.assertEqual(response.status_code, 200)
         
-class htmlTests(TestCase):
-    def tests_testhtmlpage(self):
-        response = self.client.get('/mywatchlist/html/')
-        self.assertEqual(response.status_code, 200)
+# class htmlTests(TestCase):
+#     def tests_testhtmlpage(self):
+#         response = self.client.get('/mywatchlist/html/')
+#         self.assertEqual(response.status_code, 200)
